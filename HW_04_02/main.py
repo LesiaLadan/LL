f09863462552c14b296bc86858ca9eef9be64000
Lesia <ladanlesya@gmail.com>
2026-01-18 18:18:09 +0200

    HW_05_01

--- HW_04_02/main.py
+++ HW_04_02/main.py
@@ -4,14 +4,14 @@ lst = [0, 1, 7, 2, 4, 8]
 # lst =[]
 # lst = [9, 7, 96, 0]
 
-if not lst:
-    result = 0
-    print (result) 
-else:
-    result = sum([x for i, x in enumerate(lst) if i % 2 == 0]) * lst[-1]
-    print (result)
+# if not lst:
+#     result = 0
+#     print (result) 
+# else:
+#     result = sum([x for i, x in enumerate(lst) if i % 2 == 0]) * lst[-1]
+#     print (result)
 
-# Alternative solution
+# # Alternative solution
 
 # if not lst:
 #     result = 0
@@ -19,3 +19,9 @@ else:
 # else:
 #     result = sum([x for x in lst[::2]]) * lst[-1]
 #     print (result)
+number = 0
+while number <= 10:
+    number = number + 2
+    if number == 5:
+        break 
+    print(number, end=', ')
---
+++ HW_05_01/main.py
@@ -0,0 +1,34 @@
+# _ => True
+# __ => False
+# ___ => False
+# x => True
+# get_value => True
+# get value => False
+# get!value => False
+# some_super_puper_value => True
+# Get_value => False
+# get_Value => False
+# getValue => False
+# 3m => False
+# m3 => True
+# assert => False
+# assert_exception => True
+
+
+
+import keyword
+
+x = input('Enter a string to check if it valid for variable name: \n')
+
+invalid_underscores = x.startswith("__") and x.endswith("__")
+
+var_ok = (
+    x
+    and not x[0].isdigit()
+    and x not in keyword.kwlist
+    and not invalid_underscores
+    and " " not in x
+    and all(c.islower() or c.isdigit() or c == "_" for c in x)
+)
+
+print(var_ok)

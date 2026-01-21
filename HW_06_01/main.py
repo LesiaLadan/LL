2cd9732386df9cfdaf1311fa605a69cc5b8c13de
Lesia <ladanlesya@gmail.com>
2026-01-21 09:24:49 +0200

    HW_06_01

---
+++ HW_06_01/main.py
@@ -0,0 +1,14 @@
+import string
+
+letters = string.ascii_letters
+
+user_input = input("Enter letter range (e.g. a-f):\n")
+
+start, end = user_input.split("-")
+
+start_idx = letters.index(start)
+end_idx = letters.index(end)
+
+result = letters[start_idx : end_idx + 1]
+
+print(result)

4775888eb74fa452667d092a475a54ed167650b1
Lesia <ladanlesya@gmail.com>
2026-02-05 08:52:12 +0200

    HW_10_03/main.py

--- HW_10_03/main.py
+++ HW_10_03/main.py
@@ -6,5 +6,8 @@ def is_even(digit):
 assert is_even(2) == True, 'Test1'
 assert is_even(5) == False, 'Test2'
 assert is_even(0) == True, 'Test3'
+assert is_even(2494563894038**2) == True, 'Test1'
+assert is_even(1056897**2) == False, 'Test2'
+assert is_even(24945638940387**3) == False, 'Test3'
 print('OK')
 
